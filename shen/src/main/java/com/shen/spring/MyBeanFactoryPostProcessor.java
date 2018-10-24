8d3c3c1638b7a2c9bbba3122e7372877aa654c70
shenyang <shen.5109256@qq.com>
2018-10-24 10:17:07 +0800

    spring生命周期

---
+++ shen/src/main/java/com/shen/spring/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,41 @@
+package com.shen.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    public MyBeanFactoryPostProcessor() {
+        super();
+        System.out.println("这是BeanFactoryPostProcessor实现类构造器！！");
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory arg0)
+            throws BeansException {
+        System.out
+                .println("BeanFactoryPostProcessor调用postProcessBeanFactory方法");
+        BeanDefinition bd = arg0.getBeanDefinition("person");
+        bd.getPropertyValues().addPropertyValue("phone", "110");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("现在开始初始化容器");
+
+        ApplicationContext factory = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("容器初始化成功");
+        //得到Preson，并使用
+        Person person = factory.getBean("person",Person.class);
+        System.out.println(person);
+        System.out.println(person.getPersonTwo());
+
+        System.out.println("现在开始关闭容器！");
+        ((ClassPathXmlApplicationContext)factory).registerShutdownHook();
+    }
+
+}
---
+++ shen/src/main/java/com/shen/spring/Person.java
@@ -0,0 +1,108 @@
+package com.shen.spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author qsk
+ */
+public class Person implements BeanFactoryAware, BeanNameAware,
+        InitializingBean, DisposableBean {
+
+    private String name;
+    private String address;
+    private int phone;
+
+    @Autowired
+    private PersonTwo personTwo;
+
+
+    private BeanFactory beanFactory;
+    private String beanName;
+
+    public Person() {
+        System.out.println("【构造器】调用Person的构造器实例化");
+//        System.out.println("【构造器】调用Person的构造器实例化--------"+personTwo);
+//        this.personTwo = personTwo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        System.out.println("【注入属性】注入属性name");
+        this.name = name;
+    }
+
+    public PersonTwo getPersonTwo() {
+        return personTwo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        System.out.println("【注入属性】注入属性address");
+        this.address = address;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        System.out.println("【注入属性】注入属性phone");
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person [address=" + address + ", name=" + name + ", phone="
+                + phone + "]";
+    }
+
+    // 这是BeanFactoryAware接口方法
+    @Override
+    public void setBeanFactory(BeanFactory arg0) throws BeansException {
+        System.out
+                .println("【BeanFactoryAware接口】调用BeanFactoryAware.setBeanFactory()");
+        this.beanFactory = arg0;
+    }
+
+    // 这是BeanNameAware接口方法
+    @Override
+    public void setBeanName(String arg0) {
+        System.out.println("【BeanNameAware接口】调用BeanNameAware.setBeanName()");
+        this.beanName = arg0;
+    }
+
+    // 这是InitializingBean接口方法
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out
+                .println("【InitializingBean接口】调用InitializingBean.afterPropertiesSet()");
+    }
+
+    // 这是DiposibleBean接口方法
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("【DiposibleBean接口】调用DiposibleBean.destory()");
+    }
+
+    // 通过<bean>的init-method属性指定的初始化方法
+    public void myInit() {
+        System.out.println("【init-method】调用<bean>的init-method属性指定的初始化方法");
+    }
+
+    // 通过<bean>的destroy-method属性指定的初始化方法
+    public void myDestory() {
+        System.out.println("【destroy-method】调用<bean>的destroy-method属性指定的初始化方法");
+    }
+}
