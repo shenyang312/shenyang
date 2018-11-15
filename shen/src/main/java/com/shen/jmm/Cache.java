c6e5b764550d6513737881afb9008111c7d90490
shenyang <shen.5109256@qq.com>
2018-11-15 14:27:32 +0800

    1111

--- shen/src/main/java/com/shen/jmm/Cache.java
+++ shen/src/main/java/com/shen/jmm/Cache.java
@@ -41,4 +41,43 @@ public class Cache {
             w.unlock();
         }
     }
+
+        public static void main(String[] args) throws InterruptedException {
+            RunThread thread = new RunThread();
+
+            thread.start();
+            Thread.sleep(1000);
+            thread.setRunning(false);
+
+            System.out.println("已经赋值为false");
+        }
+
+    static class RunThread extends Thread {
+        private boolean isRunning = true;
+        int m;
+        public boolean isRunning() {
+            return isRunning;
+        }
+        public void setRunning(boolean isRunning) {
+            this.isRunning = isRunning;
+        }
+        @Override
+        public void run() {
+            System.out.println("进入run了");
+            while (isRunning == true) {
+                int a=2;
+                int b=3;
+                int c=a+b;
+                m=c;
+                try {
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//                System.out.println(m);
+            }
+            System.out.println(m);
+            System.out.println("线程被停止了！");
+        }
+    }
 }
